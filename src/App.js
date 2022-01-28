bb83561640f21447e393400f37edfac71ffa1fac
ketanprabhu <ketan.prabhu@gigvistas.com>
2022-01-28 21:20:40 +0530

    new

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import Todo from "./components/Todo";
 function App() {
   return (
     <div className="app">
-      <h1>To-Do</h1>
-      <Todo title="React" text="liberary" />
-      <Todo title="JS" text="Java Script" />
-      <Todo title="Node" text="Node JS" />
-      <Todo title="To-Do" text="APP Discription" />
+
     </div>
   );
 }
